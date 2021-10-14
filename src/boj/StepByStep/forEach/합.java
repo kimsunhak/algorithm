78eea1e04b75901786e6899d04ae6ef5ee5fa662
kimsunhak <kkyj3954@ifcommunity.co.kr>
2021-10-14 11:23:08 +0900

    * 문제 8393번 합 - 수정

--- src/boj/StepByStep/forEach/합.java
+++ src/boj/StepByStep/forEach/합.java
@@ -2,14 +2,25 @@ package boj.StepByStep.forEach;
 
 import java.util.Scanner;
 
+/**
+ * 문제번호 : 8393
+ */
 public class 합 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+
+        // 시간복잡도 O(n)
         int sum = 0;
         for (int i=1; i<=n; ++i) {
             sum += i;
         }
         System.out.print(sum);
+
+        // 시간복잡도 O(1)
+        int sum2 = (n * (n+1) / 2);
+        System.out.print(sum2);
+
     }
 }
